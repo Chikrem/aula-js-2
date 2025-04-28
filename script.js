daa73d037200364b95309265461d7a3ebe9cdf51
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-04-28 18:16:25 -0300

    Estruturas de repetição (for) e escopo

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
 // array para armazenar as tarefas (declarado fora da função)
 let listaDeTarefas = []
 
+const listaTarefas = document.getElementById("listaTarefas")
+const inputTarefa = document.getElementById("inputTarefa")
+const campoMensagem = document.getElementById("mensagem")
+
 function adicionarTarefa() {
     //recebe valor do input do usuário
-    const listaTarefas = document.getElementById("listaTarefas")
-    const inputTarefa = document.getElementById("inputTarefa")
-    const campoMensagem = document.getElementById("mensagem")
     let tarefa = inputTarefa.value.trim()
-
-    listaTarefas.innerHTML = "" // limpa a lista antes de adicionar a nova tarefa
-
+    
+    const mensagemErro = "Por favor, insira uma tarefa!"
+    const mensagem = "Tarefa adicionada com sucesso!"
+    
+    
     //checa se o input está vazio
     if (tarefa === "") {
-        const mensagemErro = "Por favor, insira uma tarefa!"
         campoMensagem.textContent  = mensagemErro
         campoMensagem.style.color = "red"
         return
     } 
-
+    
     //mensagem de tarefa adicionada com sucesso
-    const mensagem = "Tarefa adicionada com sucesso!"
     campoMensagem.textContent = mensagem
     campoMensagem.style.color = "green"
     listaDeTarefas.push(tarefa) // adiciona a tarefa ao array
     console.log(listaDeTarefas)
-
+    
     //cria novo item (li) e insere na (lista ul)
+    renderTarefas()
+    
+    //limpa o input do usuário
+    inputTarefa.value = "" 
+}
+
+function renderTarefas() {
+    
+    listaTarefas.innerHTML = "" // limpa a lista ul antes de adicionar a nova tarefa
+    
     listaDeTarefas.forEach(tarefa => {
         const novaTarefa = document.createElement("li")
         novaTarefa.textContent = tarefa
         listaTarefas.appendChild(novaTarefa)
-    });
-
-    //limpa o input do usuário
-    inputTarefa.value = ""
+    })
 }
