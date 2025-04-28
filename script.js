2e10a9d84b05f89d124121bc23bf54ec0b4c5dcd
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-04-28 17:00:47 -0300

    Update

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 function adicionarTarefa() {
 
     //recebe valor do input do usuário
-    let inputTarefa = document.getElementById("inputTarefa")
-    let tarefa = inputTarefa.value
+    const inputTarefa = document.getElementById("inputTarefa")
+    let tarefa = inputTarefa.value.trim()
+    const campoMensagem = document.getElementById("mensagem")
 
-    //cria novo item (li) e insere na (lista ul)
-    let listaTarefas = document.getElementById("listaTarefas")
-    let novaTarefa = document.createElement("li")
-    novaTarefa.textContent = tarefa
-    listaTarefas.appendChild(novaTarefa)
+    //checa se o input está vazio
+    if (tarefa === "") {
+        const mensagemErro = "Por favor, insira uma tarefa!"
+        campoMensagem.textContent  = mensagemErro
+        campoMensagem.style.color = "red"
+        return
+    } 
 
     //mensagem de tarefa adicionada com sucesso
-    let mensagem = "Tarefa adicionada com sucesso!"
-    document.getElementById("mensagem").textContent = mensagem
+    const mensagem = "Tarefa adicionada com sucesso!"
+    campoMensagem.textContent = mensagem
+    campoMensagem.style.color = "green"
+
+
+    //cria novo item (li) e insere na (lista ul)
+    const listaTarefas = document.getElementById("listaTarefas")
+    const novaTarefa = document.createElement("li")
+    novaTarefa.textContent = tarefa
+    listaTarefas.appendChild(novaTarefa)
 
     //limpa o input do usuário
     inputTarefa.value = ""
